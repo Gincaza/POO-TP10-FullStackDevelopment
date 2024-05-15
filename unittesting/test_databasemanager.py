788eb5616b612b5b510536800cf19a5386d9635a
moth <dinocruz434@gmail.com>
2024-05-15 17:47:49 +0100

    Test Client
    Testando o database manager. Dentro da pasta de testing tem um mock do databasemanager, é apenas uma copia do database manager feita pra testar a partir dali

---
+++ unittesting/test_databasemanager.py
@@ -0,0 +1,16 @@
+import unittest
+from databasemockmanager import DatabaseManager
+
+
+class DatabaseManagerTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.database = DatabaseManager("teste")
+        # Limpar a tabela de clientes antes de cada teste
+
+    def test_insert_table_clients_returns_rows_clients(self):
+        # Configurar um cliente
+        self.database.insert_table_clientes("Gustavo", "Faro", 913528755)
+        # Executar o método que está sendo testado
+        clients = self.database.get_all_table_clients()
+        # Verificar se o resultado está conforme o esperado
+        self.assertEqual(clients, [(1, "Gustavo", "Faro", "913528755")])
