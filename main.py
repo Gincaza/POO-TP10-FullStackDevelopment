a10802a2b67fb842cdfc60e826f31f9f98575e05
moth <dinocruz434@gmail.com>
2024-06-05 10:51:03 +0100

    Funcionalidades basicas do mainscreen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
 from kivy.lang import Builder
 import requests
+import json
 
 Builder.load_file('login.kv')
 
@@ -23,7 +24,64 @@ class LoginScreen(Screen):
             print(f"Error: {e}")
 
 class MainScreen(Screen):
-    pass
+    def obter_clientes(self):
+        url = "http://127.0.0.1:5000/cliente"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                clientes = response.json()
+                print("Clientes:", json.dumps(clientes, indent=4))
+            else:
+                print("Failed to obtain clients")
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+
+    def adicionar_cliente(self):
+        url = "http://127.0.0.1:5000/cliente"
+        data = {
+            "nome": "Novo Cliente",
+            "morada": "Endereço",
+            "telefone": "123456789"
+        }
+        try:
+            response = requests.post(url, json=data)
+            if response.status_code == 201:
+                print("Cliente adicionado com sucesso!")
+            else:
+                print("Falha ao adicionar cliente")
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+
+    def obter_hamburgueres(self):
+        url = "http://127.0.0.1:5000/hamburguer"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                hamburgueres = response.json()
+                print("Hamburgueres:", json.dumps(hamburgueres, indent=4))
+            else:
+                print("Failed to obtain hamburgers")
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+
+    def registrar_pedido(self):
+        url = "http://127.0.0.1:5000/pedido"
+        data = {
+            "id_cliente": 1,
+            "nome_hamburguer": "Cheeseburger",
+            "quantidade": 2,
+            "tamanho": "Grande",
+            "valor_total": 20.0,
+            "data_hora": "2023-01-01 12:00:00"
+        }
+        try:
+            response = requests.post(url, json=data)
+            if response.status_code == 201:
+                print("Pedido registrado com sucesso!")
+            else:
+                print("Falha ao registrar pedido")
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
 
 class LoginApp(App):
     def build(self):
