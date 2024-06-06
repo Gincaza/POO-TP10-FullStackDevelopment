974e2838bbe69a1e3632d40191815821de1e0d85
moth <dinocruz434@gmail.com>
2024-06-06 17:06:07 +0100

    testando o update_client

--- test_app.py
+++ test_app.py
@@ -42,5 +42,39 @@ class TestClienteRoute(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.get_json(), {"erro": "Erro ao acessar o banco de dados"})
 
+class TestAtualizarClienteRoute(unittest.TestCase):
+    def setUp(self):
+        # Configurar a aplicação para o modo de teste
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
+    @patch.object(database_context, 'update_cliente')
+    @patch.object(database_context, 'get_cliente')
+    def test_atualizar_cliente_sucesso(self, mock_get_cliente, mock_update_cliente):
+        # Mocking a resposta do banco de dados
+        mock_update_cliente.return_value = None  # update_cliente não retorna nada
+        mock_get_cliente.return_value = (1, 'Cliente Atualizado', 'Nova Morada', '123456789')
+
+        dados = {
+            "id_cliente": 1,
+            "nome": "Cliente Atualizado",
+            "morada": "Nova Morada",
+            "telefone": "123456789"
+        }
+
+        response = self.client.put('/cliente', json=dados)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.get_json(), {
+            "mensagem": "Cliente atualizado com sucesso",
+            "dados": {
+                "id_cliente": 1,
+                "nome": "Cliente Atualizado",
+                "morada": "Nova Morada",
+                "telefone": "123456789"
+            }
+        })
+
+
+
 if __name__ == '__main__':
     unittest.main()
