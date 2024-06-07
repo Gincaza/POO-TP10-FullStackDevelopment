5fb9beedf9a37658dc90f4625c190fa803d40ce7
moth <dinocruz434@gmail.com>
2024-06-07 09:37:51 +0100

    Criando TestLoginRoute

--- test_app.py
+++ test_app.py
@@ -367,5 +367,11 @@ class TestDeletarHamburguerRoute(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.get_json(), {"erro": "Nome do hamburguer não fornecido"})
 
+class TestLoginRoute(unittest.TestCase):
+    def setUp(self):
+        # Configurar a aplicação para o modo de teste
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
 if __name__ == '__main__':
     unittest.main()
